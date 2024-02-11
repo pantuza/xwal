22ce8d918a8211917ad6a374ca249f706f2c1aed
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-02-10 23:33:09 -0300

    Creates a simple executable using the library

---
+++ cmd/xwal/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/pantuza/xwal/internal/xwal"
+	"github.com/pantuza/xwal/protobuf/xwalpb"
+)
+
+func main() {
+	fmt.Println("xWAL library")
+	entry := &xwalpb.WALEntry{
+
+		LSN:  42,
+		Data: []byte("fake data"),
+		CRC:  42,
+	}
+	spew.Dump(entry)
+
+	xwal, err := xwal.NewXWAL(xwal.XWALConfig{})
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	spew.Dump(xwal)
+
+}
