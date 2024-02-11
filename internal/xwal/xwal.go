b085b87caaed34d7662033e2487ecbb713db7291
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-02-10 23:32:40 -0300

    Creates structure to be the Cross Write Ahead Log

---
+++ internal/xwal/xwal.go
@@ -0,0 +1,21 @@
+package xwal
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type XWAL struct {
+	cfg    XWALConfig
+	lock   sync.RWMutex
+	ctx    context.Context
+	cancel context.CancelFunc
+	timer  time.Ticker
+}
+
+func NewXWAL(cfg XWALConfig) (*XWAL, error) {
+	var xwal XWAL
+	xwal.cfg = cfg
+	return &xwal, nil
+}
