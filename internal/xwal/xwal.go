352010887fe21647038c7299a3ad1e4aad1d6e6c
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-02-13 20:37:32 -0300

    Starts implementing XWAL struct methods

--- internal/xwal/xwal.go
+++ internal/xwal/xwal.go
@@ -4,22 +4,45 @@ import (
 	"context"
 	"sync"
 	"time"
+
+	"github.com/pantuza/xwal/internal/buffer"
+	"github.com/pantuza/xwal/pkg/types"
 )
 
 type XWAL struct {
-	cfg    XWALConfig
-	lock   sync.RWMutex
-	ctx    context.Context
-	cancel context.CancelFunc
-	timer  time.Ticker
+	cfg           XWALConfig
+	lock          sync.RWMutex
+	ctx           context.Context
+	cancel        context.CancelFunc
+	FlushInterval time.Ticker
+	buffer        *buffer.InMemoryBuffer
+	backend       types.WALBackendInterface
 }
 
 func NewXWAL(cfg XWALConfig) (*XWAL, error) {
-	var xwal XWAL
-	xwal.cfg = cfg
-	return &xwal, nil
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wal := &XWAL{
+		cfg:           cfg,
+		lock:          sync.RWMutex{},
+		ctx:           ctx,
+		cancel:        cancel,
+		FlushInterval: *time.NewTicker(cfg.FlushFrequency),
+		buffer:        buffer.NewInMemoryBuffer(cfg.BufferSize, cfg.SegmentsSize),
+	}
+
+	wal.loadBackend()
+
+	return wal, nil
 }
 
-func (wal *XWAL) configure() error {
-	return nil
+func (wal *XWAL) loadBackend() {
+	switch wal.cfg.WALBackend {
+	case types.LocalFileSystemWALBackend:
+		wal.backend = nil
+	case types.AWSS3WALBackend:
+		wal.backend = nil
+	default:
+		wal.backend = nil
+	}
 }
