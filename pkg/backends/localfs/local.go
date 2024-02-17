d719293e7be504e2080d19f950c73ca92caedb48
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-02-17 12:28:19 -0300

    Switch from EncodeVarint and DecodeVarint to new Protobuf interface MarshalTo and UnmarshalFrom

--- pkg/backends/localfs/local.go
+++ pkg/backends/localfs/local.go
@@ -117,31 +117,17 @@ func (wal *LocalFSWALBackend) extractSegmentIndex(filename string) uint32 {
 
 // Write writes the entries to the current segment file.
 func (wal *LocalFSWALBackend) Write(entries []*xwalpb.WALEntry) error {
-
 	// Initialize a bytes buffer to accumulate the serialized data
 	var buffer bytes.Buffer
 
 	for _, entry := range entries {
-
-		data, err := proto.Marshal(entry)
-		if err != nil {
-			return errors.New(fmt.Sprintf("Error Marshaling entries before writing to segment file. Error: %s", err))
-		}
-
-		// Encode the length of the entry data as a varint and write it to the buffer
-		lenBuf := proto.EncodeVarint(uint64(len(data)))
-		if _, err := buffer.Write(lenBuf); err != nil {
-			return errors.New(fmt.Sprintf("Error encoding entry length before writing to segment file. Error: %s", err))
-		}
-
-		// Write the entry data to the buffer
-		if _, err := buffer.Write(data); err != nil {
-			return errors.New(fmt.Sprintf("Error writing serialized entry as bytes before writing to segment file. Error: %s", err))
+		if _, err := protodelim.MarshalTo(&buffer, entry); err != nil {
+			return fmt.Errorf("Error marshaling entry to segment buffer before writing to file. Error: %s", err)
 		}
 	}
 
 	if _, err := buffer.WriteTo(wal.currentSegmentFile); err != nil {
-		return errors.New(fmt.Sprintf("Error writing all entries to segment file. Error: %s", err))
+		return fmt.Errorf("Error writing all entries to segment file. Error: %s", err)
 	}
 
 	return nil
@@ -193,37 +179,24 @@ func (wal *LocalFSWALBackend) getSegmentsFilesFromRange(start, end uint32) ([]st
 
 // Reads entries from a file into a slice of WALEntry.
 func (wal *LocalFSWALBackend) readEntriesFromFile(file *os.File) ([]*xwalpb.WALEntry, error) {
-
 	var entries []*xwalpb.WALEntry
 
 	fileData, err := ioutil.ReadAll(file)
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("Error reading segment file. Error: %s", err))
+		return nil, fmt.Errorf("Error reading segment file. Error: %s", err)
 	}
 
-	for len(fileData) > 0 {
-		// Decode the length of the next entry (varint).
-		len64, n := proto.DecodeVarint(fileData)
-		if n <= 0 {
-			return nil, errors.New(fmt.Sprintf("Error reading entry length from segment file. Error: %s", err))
-		}
-		fileData = fileData[n:] // Advance past the varint
+	reader := bytes.NewReader(fileData)
 
-		if len(fileData) < int(len64) {
-			return nil, errors.New(fmt.Sprintf("Error reading entry data from segment file. Error: remaining file size should not be smaller than entry length: %d < %d", len(fileData), int(len64)))
-		}
+	for reader.Len() > 0 {
 
-		// Deserialize the entry.
-		entryData := fileData[:len64]
 		entry := &xwalpb.WALEntry{}
-		if err := proto.Unmarshal(entryData, entry); err != nil {
-			return nil, errors.New(fmt.Sprintf("Error unmarshaling entry from segment file. Error: %s", err))
+
+		if err := protodelim.UnmarshalFrom(reader, entry); err != nil {
+			return nil, fmt.Errorf("Error unmarshaling entry from segment file. Error: %s", err)
 		}
 
 		entries = append(entries, entry)
-
-		// Advance the slice past this entry's data for the next iteration.
-		fileData = fileData[len64:]
 	}
 
 	return entries, nil
