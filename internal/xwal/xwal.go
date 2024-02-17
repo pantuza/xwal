3dd0f2bcc0a09a62e07ae1b1c3c73af755ff11e7
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-02-17 10:09:03 -0300

    Adds methods for Write and Flush from any WAL Backend in a thread safe fashion

--- internal/xwal/xwal.go
+++ internal/xwal/xwal.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pantuza/xwal/internal/buffer"
 	"github.com/pantuza/xwal/pkg/backends/localfs"
 	"github.com/pantuza/xwal/pkg/types"
+	"github.com/pantuza/xwal/protobuf/xwalpb"
 )
 
 type XWAL struct {
@@ -59,11 +60,75 @@ func (wal *XWAL) PeriodicFlush() {
 		select {
 		case <-wal.FlushInterval.C:
 			wal.lock.Lock()
-			wal.buffer.Flush()
+			if err := wal.flushToBackend(); err != nil {
+				fmt.Println(err) // TODO: Log error properly
+			}
 			wal.lock.Unlock()
 
 		case <-wal.ctx.Done():
 			return
 		}
 	}
+}
+
+func (wal *XWAL) Write(entry *xwalpb.WALEntry) error {
+
+	wal.lock.Lock()
+	defer wal.lock.Unlock()
+
+	return wal.writeOrFlush(entry)
+}
+
+func (wal *XWAL) WriteBatch(entries []*xwalpb.WALEntry) error {
+	wal.lock.Lock()
+	defer wal.lock.Unlock()
+
+	for _, entry := range entries {
+
+		if err := wal.writeOrFlush(entry); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (wal *XWAL) writeOrFlush(entry *xwalpb.WALEntry) error {
+
+	// If the buffer is full, flush it
+	if err := wal.buffer.Write(entry); err.Error() == buffer.ErrorShouldFlushBuffer {
+
+		// Flushes the In Memory Buffer and Writes to the WAL Backend
+		if err := wal.flushToBackend(); err != nil {
+			return err
+		}
+
+		// Writes the current entry
+		if err := wal.buffer.Write(entry); err != nil {
+			return err // If we fail again, it seems we indeed have a problem
+		}
+	}
+
+	return nil
+}
+
+func (wal *XWAL) flushToBackend() error {
+
+	// Flushes the In Memory Buffer and Writes to the WAL Backend
+	entriesToPersist := wal.buffer.Flush()
+
+	// Asynchronously writes to the backend
+	go func() {
+		if err := wal.backend.Write(entriesToPersist); err != nil {
+			fmt.Println(err) // TODO: Log error properly
+		}
+	}()
+
+	return nil
+}
+
+func (wal *XWAL) Close() error {
+	wal.cancel()
+	wal.FlushInterval.Stop()
+	return wal.backend.Close()
 }
