97805f31fb3dfb40426ff8344f7f4db9ef95f0b7
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-02-23 22:55:57 -0300

    Creates Benchmark for local file system

---
+++ benchmark/localfs_test.go
@@ -0,0 +1,38 @@
+package benchmark
+
+import (
+	"testing"
+
+	"github.com/pantuza/xwal/internal/xwal"
+	"github.com/pantuza/xwal/protobuf/xwalpb"
+)
+
+func BenchmarkLocalFSReplay(b *testing.B) {
+	cfg := xwal.NewXWALConfig("")
+	cfg.BufferSize = 1
+	cfg.BufferEntriesLength = 5
+	wal, err := xwal.NewXWAL(cfg)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	entry := &xwalpb.WALEntry{
+		LSN:  42,
+		Data: []byte("fake data"),
+		CRC:  42,
+	}
+
+	for i := 0; i < 12; i++ {
+		if err := wal.Write(entry); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := wal.Replay()
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
