bfb1869f84a02b9ba0b4363b65927d241105f8e7
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-03-22 19:32:00 -0300

    Writes test for checking Checksum calculation idempotency

--- protobuf/xwalpb/wal_entry_test.go
+++ protobuf/xwalpb/wal_entry_test.go
@@ -17,3 +17,20 @@ func TestChecksum(t *testing.T) {
 	assert.NoError(t, err, "no error expected")
 	assert.Equal(t, uint32(4049352028), entry.CRC, "expected checksum")
 }
+
+func TestChecksumIdempotency(t *testing.T) {
+	entry := &WALEntry{
+		LSN:  1,
+		Data: []byte("test data"),
+	}
+
+	var err error
+	entry.CRC, err = entry.Checksum()
+	assert.NoError(t, err, "no error expected")
+	assert.Equal(t, uint32(4049352028), entry.CRC, "expected checksum")
+
+	// Calculate the checksum again.
+	chksum, err := entry.Checksum()
+	assert.NoError(t, err, "no error expected")
+	assert.Equal(t, chksum, entry.CRC, "expected checksum")
+}
