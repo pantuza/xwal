5788127a339d015ea850d772b44d15a2b8ae99c3
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-03-22 19:31:20 -0300

    Make sure CRC Calculation does not takes in consideration the CRC field itself. Makes this function idempotent

--- protobuf/xwalpb/wal_entry.go
+++ protobuf/xwalpb/wal_entry.go
@@ -12,11 +12,18 @@ import (
 // which are assumed to be the checksum itself.
 // The implementation uses crc32 ChecksumIEEE as the CRC algorithm.
 func (w *WALEntry) Checksum() (uint32, error) {
+	// Store the current CRC value and set it to 0 to calculate the Checksum.
+	// It protects the CRC field from being included in the checksum calculation.
+	storedCRC := w.CRC
+	w.CRC = 0
 
 	marshaledProto, err := proto.Marshal(w)
 	if err != nil {
 		return 0, fmt.Errorf("failed to marshal WAL entry: %v", err)
 	}
+
+	// Restore the original CRC value.
+	w.CRC = storedCRC
 
 	return crc32.ChecksumIEEE(marshaledProto), nil
 }
