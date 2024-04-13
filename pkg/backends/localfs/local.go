244fed98e32b51e1687e28637f9829d20a031057
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-04-12 22:45:52 -0300

    Adds new configuration option on LocalFS Backend: SegmentsDirSizeGB

--- pkg/backends/localfs/local.go
+++ pkg/backends/localfs/local.go
@@ -39,6 +39,9 @@ type LocalFSConfig struct {
 	// Size in megabytes of each file inside the WAL
 	SegmentsFileSize int `yaml:"segmentsFileSize"`
 
+	// Size in Gigabytes of the Segments Directory
+	SegmentsDirSizeGB uint32 `yaml:"segmentsDirSizeGB"`
+
 	// Interval to clean garbage logs
 	CleanLogsInterval time.Duration `yaml:"cleanLogsInterval"`
 }
