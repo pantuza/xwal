3b981ee159f8f3d60e9a573f7d310536f18e27f5
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-08-17 21:33:26 -0300

    Starts the implementation of the AWS s3 WAL example code
    Signed-off-by: Gustavo Pantuza <gustavopantuza@gmail.com>

---
+++ examples/awss3/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/pantuza/xwal/internal/xwal"
+	"github.com/pantuza/xwal/protobuf/xwalpb"
+)
+
+func main() {
+	fmt.Println("xWAL library")
+
+	cfg := xwal.NewXWALConfig("")
+	cfg.BufferSize = 1
+	cfg.BufferEntriesLength = 5
+
+	xwal, err := xwal.NewXWAL(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer xwal.Close()
+
+	for i := 0; i < 12; i++ {
+
+		fmt.Printf("Writing entry %d\n", i)
+
+		data := []byte(`{"name": "John", "age": 42, "city": "Belo Horizonte"}`) // any []byte fake data
+		if err := xwal.Write(data); err != nil {
+			panic(err)
+		}
+	}
+
+	err = xwal.Replay(func(entries []*xwalpb.WALEntry) error {
+		for _, entry := range entries {
+			spew.Dump(entry)
+		}
+		return nil // Return nil or an appropriate error value
+	}, 5, false)
+	if err != nil {
+		panic(err)
+	}
+	// spew.Dump(xwal)
+}
